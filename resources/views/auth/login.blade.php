a08a241583d7d1b02c44f4c2bf5b87efeb029fc9
kuga24 <galihkuncoro19@gmail.com>
2025-05-06 10:36:43 +0700

    remove register button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -86,28 +86,28 @@ License: For each use you must have a valid license purchased only from above li
                                 <h1 class="text-dark fw-bolder mb-3">Sign In</h1>
                                 <!--end::Title-->
                                 <!--begin::Subtitle-->
-                                <div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
+                                <!-- <div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div> -->
                                 <!--end::Subtitle=-->
                             </div>
                             <!--begin::Heading-->
                             <!--begin::Login options-->
                             <div class="row g-3 mb-9">
                                 <!--begin::Col-->
-                                <div class="col-md-6">
-                                    <!--begin::Google link=-->
+                                <!-- <div class="col-md-6">
+                                    begin::Google link=
                                     <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                                         <img alt="Logo" src="{{ asset('media/svg/brand-logos/google-icon.svg') }}" class="h-15px me-3" />Sign in with Google</a>
-                                    <!--end::Google link=-->
-                                </div>
+                                    end::Google link=
+                                </div> -->
                                 <!--end::Col-->
                                 <!--begin::Col-->
-                                <div class="col-md-6">
-                                    <!--begin::Google link=-->
+                                <!-- <div class="col-md-6">
+                                    begin::Google link=
                                     <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                                         <img alt="Logo" src="{{ asset('media/svg/brand-logos/apple-black.svg') }}" class="theme-light-show h-15px me-3" />
                                         <img alt="Logo" src="{{ asset('media/svg/brand-logos/apple-black-dark.svg') }}" class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
-                                    <!--end::Google link=-->
-                                </div>
+                                    end::Google link=
+                                </div> -->
                                 <!--end::Col-->
                             </div>
                             <!--end::Login options-->
@@ -154,9 +154,6 @@ License: For each use you must have a valid license purchased only from above li
                             </div>
                             <!--end::Submit button-->
                             <!--begin::Sign up-->
-                            <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
-                                <a href="{{ url('/register') }}" class="link-primary">Sign up</a>
-                            </div>
                             <!--end::Sign up-->
                         </form>
                         <!--end::Form-->
